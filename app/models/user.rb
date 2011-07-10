6ee1146a09c8f35c160f020fd4ce2518f1028c02
James Dela Cruz <jamesdelacruz0409@gmail.com>
2011-07-10 12:08:24 +0800

    upload new photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessible :password
   
   attr_accessible :name, :email 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,6 +8,8 @@ class User < ActiveRecord::Base
   validates :name,  :presence => true
 		    
   validates :email, :presence => true
+  
+  validates :password :presence => true
 		   
   
 end
